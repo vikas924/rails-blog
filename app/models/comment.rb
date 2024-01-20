739586c3f064fe959378dfac37e6070fb2eb510f
vikas924 <obroy902@gmail.com>
2024-01-20 06:59:09 +0530

    Add model and methods for comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user
   belongs_to :post
+
+  attribute :text, :text
+
+  after_create :update_posts_counter
+  after_destroy :update_posts_counter
+
+  def update_posts_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
