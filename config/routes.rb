ef465935cb2b5a725e08d2ba4a466a32102e9a58
vikas924 <obroy902@gmail.com>
2024-01-27 17:22:10 +0530

    Add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 
-  resources :posts, only: [:index, :show]
+  # resources :posts, only: [:index, :show]
   root 'users#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
