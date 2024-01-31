8335174aeaa73791f8855d902031687d6b209ab2
Mesi <mse212002@gmail.com>
2024-01-31 10:46:41 +0200

    :white_checkmark: add tests for users_show

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -32,13 +32,16 @@ RSpec.feature 'User Show', type: :feature do
   let!(:post1) { Post.create(author: user, title: 'Post title 1', text: 'first text') }
   let!(:post2) { Post.create(author: user, title: 'Post title 2', text: 'second text') }
   let!(:post3) { Post.create(author: user, title: 'Post title 3', text: 'third text') }
+  
   before do
     visit user_path(user)
   end
+  
   scenario 'redirects to the post show page when you click on a users post' do
     click_link 'Post title 1'
     expect(current_path).to eq(user_post_path(user, post1))
   end
+  
   scenario 'redirects to the users posts index page when you click on "See all posts"' do
     click_link 'See All Posts'
     expected_path = user_posts_path(user).chomp('/')
