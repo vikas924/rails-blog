7bd852fc4564890d72a0dcb6408c80477f0a25a5
vikas924 <obroy902@gmail.com>
2024-02-03 13:17:03 +0530

    Add role to user

---
+++ app/models/ability.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :update, Post do |post|
+        post.author == user
+      end
+
+      can :destroy, Post do |post|
+        post.author == user
+      end
+
+      can :update, Comment do |comment|
+        comment.user == user
+      end
+
+      can :destroy, Comment do |comment|
+        comment.user == user
+      end
+
+      can :create, Post
+      can :create, Comment
+      can :read, :all
+    end
+  end
+end
