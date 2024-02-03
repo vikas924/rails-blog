b0cec6da7ea10eab44744412b315fb23996ea53e
vikas924 <obroy902@gmail.com>
2024-02-03 14:16:02 +0530

    linter fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, path: 'auth',
   path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
   devise_scope :user do
-    get '/auth/logout', to: 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sesssions#destroy'
     root to: 'devise/sessions#new'
   end
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments ,only:[:index,:new,:create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments ,only:[:index,:new,:create, :destroy]
       resources :likes ,only:[:create]
     end
   end
